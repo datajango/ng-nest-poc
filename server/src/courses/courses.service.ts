976547cac1db721658bc454a0414a76db263f917
Anthony Leotta <tony@leotta.com>
2019-04-29 13:28:18 -0400

    Adds material design to front-end

--- server/src/courses/courses.service.ts
+++ server/src/courses/courses.service.ts
@@ -1,27 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { Course } from './course';
 
+/* lorem pixel categories
+abstract
+animals
+business
+cats
+city
+food
+nightlife
+fashion
+people
+nature
+sports
+technics
+transport
+*/
+
 const courses: Course[] =[
     {
         id: '1',
         title: 'NestJS from Novice to Guru',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet neque nec nunc mollis sagittis. Pellentesque vulputate facilisis justo, quis facilisis purus imperdiet pulvinar. Sed elementum dictum dictum. In sem mauris, vestibulum quis ligula quis, eleifend pharetra lectus. Vivamus pulvinar leo in ante hendrerit, at varius risus placerat. Fusce molestie quam id lorem facilisis, vitae tempus leo consectetur.',
         author: 'John Smith',
-        length: '4 Hour 45 Minutes'
+        length: '4 Hour 45 Minutes',
+        category: 'business'
     },
     {
         id: '2',
         title: 'Angular Material Design Essentials',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet neque nec nunc mollis sagittis. Pellentesque vulputate facilisis justo, quis facilisis purus imperdiet pulvinar. Sed elementum dictum dictum. In sem mauris, vestibulum quis ligula quis, eleifend pharetra lectus. Vivamus pulvinar leo in ante hendrerit, at varius risus placerat. Fusce molestie quam id lorem facilisis, vitae tempus leo consectetur.',
         author: 'John Smith',
-        length: '3 Hour 33 Minutes'
+        length: '3 Hour 33 Minutes',
+        category: 'people'
     },
     {
         id: '3',
         title: 'Mongodb Secrets',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sit amet neque nec nunc mollis sagittis. Pellentesque vulputate facilisis justo, quis facilisis purus imperdiet pulvinar. Sed elementum dictum dictum. In sem mauris, vestibulum quis ligula quis, eleifend pharetra lectus. Vivamus pulvinar leo in ante hendrerit, at varius risus placerat. Fusce molestie quam id lorem facilisis, vitae tempus leo consectetur.',
         author: 'John Smith',
-        length: '4 Hour 12 Minutes'
+        length: '4 Hour 12 Minutes',
+        category: 'nightlife'
     }
 ];
 
